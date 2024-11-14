c535dc8a74a730cd57b25d55d87597ca8b2f7d28
shaksham56 <shaksham.sinha@yellow.ai>
2024-11-14 23:08:13 +0530

    devadded basic routing and project setup

---
+++ backend/db.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://shaksham08:AK6gnML4TN8oRCWV@cluster0.wyxt8.mongodb.net/paytm_db"
+);
+
+// AK6gnML4TN8oRCWV
+
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+});
+
+module.exports = {
+  userSchema,
+};
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,15 @@
 const express = require("express");
+const rootRouter = require("./routes/index");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
+const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/api/v1", rootRouter);
+
+app.listen(3011, () => {
+  console.log("server started at port 3011");
+});
