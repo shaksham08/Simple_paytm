9677025b82b9c4576b5ee10812f6e9abeb3f3d33
shaksham08 <shakshamsinha@outlook.com>
2024-11-24 20:50:44 +0530

    added basic routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import SignIn from "./Pages/SignIn/SignIn";
+import Signup from "./Pages/Signup/Signup";
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
-  )
+    <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+      <Route path="/send" element={<h1>Send</h1>} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
