29acce1f8c8e48c4e604846f46c13fbb7b5f4bbe
shaksham08 <shakshamsinha@outlook.com>
2024-11-24 21:40:44 +0530

    added basic styling and page layouts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import { Routes, Route } from "react-router-dom";
-import SignIn from "./Pages/SignIn/SignIn";
-import Signup from "./Pages/Signup/Signup";
+import { Signup } from "./Pages/Signup/Signup";
+import { Signin } from "./Pages/SignIn/SignIn";
+import { SendMoney } from "./Pages/SendMoney/SendMoney";
 
 function App() {
   return (
     <Routes>
       <Route path="/signup" element={<Signup />} />
-      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signin" element={<Signin />} />
       <Route path="/dashboard" element={<h1>Dashboard</h1>} />
-      <Route path="/send" element={<h1>Send</h1>} />
+      <Route path="/send" element={<SendMoney />} />
     </Routes>
   );
 }
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Ensure this matches your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
